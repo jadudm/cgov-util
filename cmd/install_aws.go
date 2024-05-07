34c38d2436842041f5c89e7973fe28b38144320f
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-05-07 13:22:04 -0400

    Make the input of byte become a string to read stdout

--- cmd/install_aws.go
+++ cmd/install_aws.go
@@ -14,7 +14,7 @@ import (
 	"gov.gsa.fac.cgov-util/internal/util"
 )
 
-func ErrorCheck(output []byte, err error) {
+func ErrorCheck(output string, err error) {
 	// https://stackoverflow.com/a/7786922
 	if err != nil {
 		logging.Logger.Println(err.Error())
