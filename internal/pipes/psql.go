1ec21a31b7b03e2891e56d4911ec2a5ed4869965
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-05-23 14:46:18 -0400

    Rename output

--- internal/pipes/psql.go
+++ internal/pipes/psql.go
@@ -27,6 +27,6 @@ func Psql(in_pipe *script.Pipe, creds vcap.Credentials) *script.Pipe {
 	if util.IsDebugLevel("DEBUG") {
 		logging.Logger.Printf("command: %s\n", combined)
 	}
-	logging.Logger.Printf("BACKUPS " + util.PSQL_path + " running\n")
+	logging.Logger.Printf(util.PSQL_path + " running\n")
 	return in_pipe.Exec(combined)
 }
