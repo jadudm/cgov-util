17b189f00fc6a0f83207c57e60ada4dc55ed60f6
Alex Steel <130377221+asteel-gsa@users.noreply.github.com>
2024-06-17 14:56:02 -0400

    Add log - STATUS

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -7,3 +7,4 @@ import (
 
 var Logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 var Error = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime)
+var Status = log.New(os.Stdout, "STATUS: ", log.Ldate|log.Ltime)
